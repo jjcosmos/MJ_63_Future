6b89cc4681aa8e3ee5f79fafa9a2647f38bcc592
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2020-09-21 00:29:16 -0700

    final push

---
+++ Assets/FmodHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FmodHelper : MonoBehaviour
+{
+    bool audioResumed = false;
+    public void LoadResources()
+    {
+        if (!audioResumed)
+        {
+            var result = FMODUnity.RuntimeManager.CoreSystem.mixerSuspend();
+            Debug.Log(result);
+            result = FMODUnity.RuntimeManager.CoreSystem.mixerResume();
+            Debug.Log(result);
+            audioResumed = true;
+        }
+        SceneManager.LoadScene("CutsceneIntro");
+    }
+
+    
+    
+}
